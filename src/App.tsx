833e9f97ae3afb5b41ea1e0368c9d6560704b06d
jfgordon <jfgordon@udel.edu>
2025-03-02 02:16:59 -0500

    prolly the best im gonna get for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Button, Container, Row, Col, Modal } from "react-bootstrap";
 import './App.css';
 
 function App() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h1> Welcome to the Task Master</h1>
+        <h1 className="header2"> Hello End User!</h1>
+      </div>
+      <Container>
+        <Row className="Row">
+          <Col className="Column">
+            <div>
+
+            </div>
+            <div className="buttonPOS">
+              <Button onClick={handleShow}> Add Task</Button>
+            </div>
+          </Col>
+          <Col className="Column">
+            <div>
+              
+            </div>
+            <div className="buttonPOS">
+              <Button> Update</Button>
+            </div>
+          </Col>
+          <Col className="Column">
+            <div>
+              
+            </div>
+            <div className='buttonPOS'>
+              <Button> Complete</Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+
+      <Modal show={show} onHide={handleClose} className='popUp'>
+        <Modal.Header closeButton>
+          <Modal.Title>Task Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Here are the details of the task...</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
